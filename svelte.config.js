4c96fdd2777011271c0e0a8e489daa775a90c03c
Aqeel Chishti <aqeel.chishti@outlook.com>
2022-11-24 18:21:35 +0500

    4th update in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 //import adapter from '@sveltejs/adapter-cloudflare';
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
 
 import preprocess from 'svelte-preprocess';
 
@@ -11,9 +11,21 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+		      // if true, will deploy the app using edge functions
+		      // (https://vercel.com/docs/concepts/functions/edge-functions)
+		      // rather than serverless functions
+		      edge: false,
+
+		      // an array of dependencies that esbuild should treat
+		      // as external when bundling functions
+		      external: [],
+
+		      // if true, will split your app into multiple functions
+		      // instead of creating a single one for the entire app
+		      split: false
+	    	})
 	},
-	
 };
 
 export default config;
